9958fa3cd7045824edf7aa99121a810ae1b135a0
Chase Warrington <Chase Warrington>
2016-10-11 17:01:40 -0400

    Fixed eating non-cooked food.

--- CookingSkill/CookingSkillMod.cs
+++ CookingSkill/CookingSkillMod.cs
@@ -230,111 +230,114 @@ namespace CookingSkill
 				        string[] info = Game1.objectInformation[obj.ParentSheetIndex].Split( '/' );
                         string[] buffData = ((Convert.ToInt32(info[2]) > 0 && info.Count() > 6) ? info[6].Split(' ') : null);
 
-                        // Need to make sure this is the original buff first.
-                        // So it doesn't get rebuffed from eating a buff food -> non buff food -> buff food or something
-                        Buff oldBuff = ( info[ 5 ] == "drink" ? Game1.buffsDisplay.drink : Game1.buffsDisplay.food );
-                        Buff thisBuff = ( buffData == null ? null : new Buff(Convert.ToInt32(buffData[0]), Convert.ToInt32(buffData[1]), Convert.ToInt32(buffData[2]), Convert.ToInt32(buffData[3]), Convert.ToInt32(buffData[4]), Convert.ToInt32(buffData[5]), Convert.ToInt32(buffData[6]), Convert.ToInt32(buffData[7]), Convert.ToInt32(buffData[8]), Convert.ToInt32(buffData[9]), Convert.ToInt32(buffData[10]), 0, (info.Count<string>() > 7) ? Convert.ToInt32(info[7]) : -1, info[0]));
-                        int[] oldAttr = (oldBuff == null ? null : ((int[]) Util.GetInstanceField( typeof( Buff ), oldBuff, "buffAttributes" )));
-                        int[] thisAttr = (thisBuff == null ? null : ((int[]) Util.GetInstanceField( typeof( Buff ), thisBuff, "buffAttributes" )));
-                        Log.Async("Ate something: " + obj + " " + Game1.objectInformation[obj.ParentSheetIndex] + " " + buffData + " " + oldBuff + " " + thisBuff + " " + oldAttr + " " + thisAttr);
-                        if ( oldBuff != null && thisBuff != null && Enumerable.SequenceEqual( oldAttr, thisAttr ) &&
-                             (( info[ 5 ] == "drink" && oldBuff != lastDrink ) || (info[ 5 ] != "drink" && oldBuff != lastDrink)))
+                        if (info[3] == "Cooking -7")
                         {
-                            // Now that we know that this is the original buff, we can buff the buff.
-                            Log.Async("Buffing buff");
-                            int[] newAttr = (int[]) thisAttr.Clone();
-                            if ( Game1.player.professions.Contains( PROFESSION_BUFFLEVEL ) )
+                            // Need to make sure this is the original buff first.
+                            // So it doesn't get rebuffed from eating a buff food -> non buff food -> buff food or something
+                            Buff oldBuff = (info[5] == "drink" ? Game1.buffsDisplay.drink : Game1.buffsDisplay.food);
+                            Buff thisBuff = (buffData == null ? null : new Buff(Convert.ToInt32(buffData[0]), Convert.ToInt32(buffData[1]), Convert.ToInt32(buffData[2]), Convert.ToInt32(buffData[3]), Convert.ToInt32(buffData[4]), Convert.ToInt32(buffData[5]), Convert.ToInt32(buffData[6]), Convert.ToInt32(buffData[7]), Convert.ToInt32(buffData[8]), Convert.ToInt32(buffData[9]), Convert.ToInt32(buffData[10]), 0, (info.Count<string>() > 7) ? Convert.ToInt32(info[7]) : -1, info[0]));
+                            int[] oldAttr = (oldBuff == null ? null : ((int[])Util.GetInstanceField(typeof(Buff), oldBuff, "buffAttributes")));
+                            int[] thisAttr = (thisBuff == null ? null : ((int[])Util.GetInstanceField(typeof(Buff), thisBuff, "buffAttributes")));
+                            Log.Async("Ate something: " + obj + " " + Game1.objectInformation[obj.ParentSheetIndex] + " " + buffData + " " + oldBuff + " " + thisBuff + " " + oldAttr + " " + thisAttr);
+                            if (oldBuff != null && thisBuff != null && Enumerable.SequenceEqual(oldAttr, thisAttr) &&
+                                 ((info[5] == "drink" && oldBuff != lastDrink) || (info[5] != "drink" && oldBuff != lastDrink)))
                             {
-                                for ( int i = 0; i < thisAttr.Length; ++i )
+                                // Now that we know that this is the original buff, we can buff the buff.
+                                Log.Async("Buffing buff");
+                                int[] newAttr = (int[])thisAttr.Clone();
+                                if (Game1.player.professions.Contains(PROFESSION_BUFFLEVEL))
                                 {
-                                    if (newAttr[i] <= 0)
-                                        continue;
+                                    for (int i = 0; i < thisAttr.Length; ++i)
+                                    {
+                                        if (newAttr[i] <= 0)
+                                            continue;
 
-                                    if (i == 7 || i == 8)
-                                        newAttr[i] = (int)(newAttr[i] * 1.2);
-                                    else
-                                        newAttr[i]++;
+                                        if (i == 7 || i == 8)
+                                            newAttr[i] = (int)(newAttr[i] * 1.2);
+                                        else
+                                            newAttr[i]++;
+                                    }
                                 }
-                            }
 
-                            int newTime = (info.Count<string>() > 7) ? Convert.ToInt32(info[7]) : -1;
-                            if ( newTime != -1 && Game1.player.professions.Contains( PROFESSION_BUFFTIME ) )
-                            {
-                                newTime = (int)(newTime * 1.25);
-                            }
+                                int newTime = (info.Count<string>() > 7) ? Convert.ToInt32(info[7]) : -1;
+                                if (newTime != -1 && Game1.player.professions.Contains(PROFESSION_BUFFTIME))
+                                {
+                                    newTime = (int)(newTime * 1.25);
+                                }
 
-                            Buff newBuff = new Buff( newAttr[0],newAttr[1],newAttr[2],newAttr[3],newAttr[4],newAttr[5], newAttr[6],newAttr[7],newAttr[8],newAttr[9],newAttr[10],newAttr[11], newTime, info[ 0 ] );
-                            newBuff.millisecondsDuration = newTime / 10 * 7000;
-                            // ^ The vanilla code decreases the duration based on the time of day.
-                            // This is fine normally, since it ends as the day ends.
-                            // However if you have something like TimeSpeed it just means it won't
-                            // last as long later if eaten later in the day.
+                                Buff newBuff = new Buff(newAttr[0], newAttr[1], newAttr[2], newAttr[3], newAttr[4], newAttr[5], newAttr[6], newAttr[7], newAttr[8], newAttr[9], newAttr[10], newAttr[11], newTime, info[0]);
+                                newBuff.millisecondsDuration = newTime / 10 * 7000;
+                                // ^ The vanilla code decreases the duration based on the time of day.
+                                // This is fine normally, since it ends as the day ends.
+                                // However if you have something like TimeSpeed it just means it won't
+                                // last as long later if eaten later in the day.
 
-                            if ( info[ 5 ] == "drink" )
-                            {
-                                Game1.buffsDisplay.drink.removeBuff();
-                                Game1.buffsDisplay.drink = newBuff;
-                                Game1.buffsDisplay.drink.addBuff();
-                                lastDrink = newBuff;
-                            }
-                            else
-                            {
-                                Game1.buffsDisplay.food.removeBuff();
-                                Game1.buffsDisplay.food = newBuff;
-                                Game1.buffsDisplay.food.addBuff();
-                                lastFood = newBuff;
-                            }
-                            Game1.buffsDisplay.syncIcons();
-                        }
-                        else if ( thisBuff == null && Game1.player.professions.Contains( PROFESSION_BUFFPLAIN ) )
-                        {
-                            Log.Async("Buffing plain");
-                            Random rand = new Random();
-                            int[] newAttr = new int[12];
-                            int count = 1 + Math.Min(obj.edibility / 30, 3);
-                            for ( int i = 0; i < count; ++i )
-                            {
-                                int attr = rand.Next(10);
-                                if (attr >= 3) ++attr; // 3 unused?
-                                if (attr >= 6) ++attr; // 6 is crafting speed, unused?
-                                
-                                int amt = 1;
-                                if (attr == 7 || attr == 8)
-                                    amt = 25 + rand.Next(4) * 5;
+                                if (info[5] == "drink")
+                                {
+                                    Game1.buffsDisplay.drink.removeBuff();
+                                    Game1.buffsDisplay.drink = newBuff;
+                                    Game1.buffsDisplay.drink.addBuff();
+                                    lastDrink = newBuff;
+                                }
                                 else
                                 {
-                                    // 36% (assuming I used this probability calculator right) chance for a buff to be level 2
-                                    // 4% chance for it to be 3
-                                    if ( rand.NextDouble() < 0.2)
-                                        ++amt;
-                                    if ( rand.NextDouble() < 0.2 )
-                                        ++amt;
+                                    Game1.buffsDisplay.food.removeBuff();
+                                    Game1.buffsDisplay.food = newBuff;
+                                    Game1.buffsDisplay.food.addBuff();
+                                    lastFood = newBuff;
                                 }
-                                newAttr[attr] += amt;
+                                Game1.buffsDisplay.syncIcons();
                             }
-
-                            int newTime = 120 + obj.edibility / 10 * 30;
-
-                            Buff newBuff = new Buff(newAttr[0], newAttr[1], newAttr[2], newAttr[3], newAttr[4], newAttr[5], newAttr[6], newAttr[7], newAttr[8], newAttr[9], newAttr[10], newAttr[11], newTime, info[0]);
-                            newBuff.millisecondsDuration = newTime / 10 * 7000;
-
-                            if (info[5] == "drink")
+                            else if (thisBuff == null && Game1.player.professions.Contains(PROFESSION_BUFFPLAIN))
                             {
-                                if (Game1.buffsDisplay.drink != null)
-                                    Game1.buffsDisplay.drink.removeBuff();
-                                Game1.buffsDisplay.drink = newBuff;
-                                Game1.buffsDisplay.drink.addBuff();
-                                lastDrink = newBuff;
+                                Log.Async("Buffing plain");
+                                Random rand = new Random();
+                                int[] newAttr = new int[12];
+                                int count = 1 + Math.Min(obj.edibility / 30, 3);
+                                for (int i = 0; i < count; ++i)
+                                {
+                                    int attr = rand.Next(10);
+                                    if (attr >= 3) ++attr; // 3 unused?
+                                    if (attr >= 6) ++attr; // 6 is crafting speed, unused?
+
+                                    int amt = 1;
+                                    if (attr == 7 || attr == 8)
+                                        amt = 25 + rand.Next(4) * 5;
+                                    else
+                                    {
+                                        // 36% (assuming I used this probability calculator right) chance for a buff to be level 2
+                                        // 4% chance for it to be 3
+                                        if (rand.NextDouble() < 0.2)
+                                            ++amt;
+                                        if (rand.NextDouble() < 0.2)
+                                            ++amt;
+                                    }
+                                    newAttr[attr] += amt;
+                                }
+
+                                int newTime = 120 + obj.edibility / 10 * 30;
+
+                                Buff newBuff = new Buff(newAttr[0], newAttr[1], newAttr[2], newAttr[3], newAttr[4], newAttr[5], newAttr[6], newAttr[7], newAttr[8], newAttr[9], newAttr[10], newAttr[11], newTime, info[0]);
+                                newBuff.millisecondsDuration = newTime / 10 * 7000;
+
+                                if (info[5] == "drink")
+                                {
+                                    if (Game1.buffsDisplay.drink != null)
+                                        Game1.buffsDisplay.drink.removeBuff();
+                                    Game1.buffsDisplay.drink = newBuff;
+                                    Game1.buffsDisplay.drink.addBuff();
+                                    lastDrink = newBuff;
+                                }
+                                else
+                                {
+                                    if (Game1.buffsDisplay.food != null)
+                                        Game1.buffsDisplay.drink.removeBuff();
+                                    Game1.buffsDisplay.drink = newBuff;
+                                    Game1.buffsDisplay.drink.addBuff();
+                                    lastFood = newBuff;
+                                }
+                                Game1.buffsDisplay.syncIcons();
                             }
-                            else
-                            {
-                                if ( Game1.buffsDisplay.food != null)
-                                    Game1.buffsDisplay.drink.removeBuff();
-                                Game1.buffsDisplay.drink = newBuff;
-                                Game1.buffsDisplay.drink.addBuff();
-                                lastFood = newBuff;
-                            }
-                            Game1.buffsDisplay.syncIcons();
                         }
                     }
                 }
