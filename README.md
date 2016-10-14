1e9165b9c9be66fded67029c8ac0f8a668310a86
Chase Warrington <Chase Warrington>
2016-10-14 12:50:16 -0400

    Base of mod.

---
+++ README.md
@@ -0,0 +1,5 @@
+# RushOrders
+This is the source code. Releases can be found at:
+* [My site](http://spacechase0.com/mods/stardew-valley/rush-orders/)
+* [Nexus](http://www.nexusmods.com/stardewvalley/mods/-/) MEOW FIX LINK
+* [Chucklefish forums](http://community.playstarbound.com/resources/-.-/) MEOW FIX LINK 
---
+++ RushOrders/RushOrdersMod.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewModdingAPI.Inheritance;
+using StardewValley;
+using StardewValley.Menus;
+using StardewValley.Objects;
+using StardewValley.Tools;
+using Object = StardewValley.Object;
+using Microsoft.Xna.Framework.Input;
+
+namespace RushOrders
+{
+    public class RushOrdersMod : Mod
+    {
+        public override void Entry(params object[] objects)
+        {
+            GameEvents.UpdateTick += onUpdate;
+        }
+
+        public static void onUpdate(object sender, EventArgs args)
+        {
+        }
+    }
+}
