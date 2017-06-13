1b5fe0e9281ee9b52d22c56665719b631890d5f7
spacechase0 <spacechase0.and.cat@gmail.com>
2017-06-13 17:11:40 -0400

    Remove wallpaper/flooring commands, SMAPI has them now. (Also add missing files)

--- Commands.cs
+++ Commands.cs
@@ -12,35 +12,6 @@ namespace SpaceCore
     {
         internal static void register()
         {
-            Command.register("player_addwallpaper", addWallpaper);
-            Command.register("player_addflooring", addFlooring);
-        }
-
-        private static void addWallpaper(string[] args)
-        {
-            addWallpaperItem(args, false);
-        }
-
-        private static void addFlooring(string[] args)
-        {
-            addWallpaperItem(args, true);
-        }
-
-        private static void addWallpaperItem(string[] args, bool isFloor)
-        {
-            if (args.Length < 1)
-            {
-                Console.WriteLine("ID parameter required");
-                return;
-            }
-            int id = 0;
-            if ( !Int32.TryParse( args[ 0 ], out id ) )
-            {
-                Console.WriteLine("Invalid ID provided");
-                return;
-            }
-
-            Game1.player.addItemByMenuIfNecessary(new Wallpaper(id, isFloor));
         }
     }
 }
