dfc98d1b63b7f08079238c2947643bf32e07508b
spacechase0 <spacechase0.and.cat@gmail.com>
2018-07-29 19:57:36 -0400

    No attribute + 1.3 update

--- Mod.cs
+++ Mod.cs
@@ -14,22 +14,43 @@ namespace ExtendedReach
     public class Mod : StardewModdingAPI.Mod
     {
         public static Mod instance;
+        private HarmonyInstance harmony;
+
         public override void Entry(IModHelper helper)
         {
-            base.Entry(helper);
             instance = this;
 
             try
             {
-                Log.debug("Patching...");
-                var harmony = HarmonyInstance.Create("spacechase0.ExtendedReach");
-                harmony.PatchAll(Assembly.GetExecutingAssembly());
-                Log.debug("Done!");
+                harmony = HarmonyInstance.Create("spacechase0.ExtendedReach");
+                doTranspiler(typeof(Utility), "canGrabSomethingFromHere");
+                doTranspiler(typeof(Utility), "checkForCharacterInteractionAtTile");
+                doTranspiler(typeof(Game1), "pressActionButton");
+                doTranspiler(typeof(Game1), "pressUseToolButton");
+                doTranspiler(typeof(Game1), "tryToCheckAt");
+                doTranspiler(typeof(GameLocation), "isActionableTile");
             }
             catch ( Exception e )
             {
                 Log.error("Exception patching: ");
                 Log.error(e.ToString());
+            }
+        }
+
+        private void doTranspiler(Type origType, string origMethod)
+        {
+            doTranspiler(origType.GetMethod(origMethod), typeof(TileRadiusFix).GetMethod("IncreaseRadiusChecks"));
+        }
+        private void doTranspiler(MethodInfo orig, MethodInfo transpiler)
+        {
+            try
+            {
+                Log.trace($"Doing transpiler patch {orig}:{transpiler}...");
+                harmony.Patch(orig, null, null, new HarmonyMethod(transpiler));
+            }
+            catch (Exception e)
+            {
+                Log.error($"Exception doing transpiler patch {orig}:{transpiler}: {e}");
             }
         }
     }
--- TileRadiusFixes.cs
+++ TileRadiusFixes.cs
@@ -11,9 +11,9 @@ using System.Threading.Tasks;
 
 namespace ExtendedReach
 {
-    internal class TileRadiusFix
+    public class TileRadiusFix
     {
-        internal static IEnumerable<CodeInstruction> IncreaseRadiusChecks(ILGenerator gen, MethodBase original, IEnumerable<CodeInstruction> insns)
+        public static IEnumerable<CodeInstruction> IncreaseRadiusChecks(ILGenerator gen, MethodBase original, IEnumerable<CodeInstruction> insns)
         {
             // TODO: Learn how to use ILGenerator
 
@@ -33,61 +33,6 @@ namespace ExtendedReach
             }
 
             return newInsns;
-        }
-    }
-
-    [HarmonyPatch(typeof(Utility), "canGrabSomethingFromHere")]
-    public class Utility_canGrabSomethingFromHere
-    {
-        internal static IEnumerable<CodeInstruction> Transpiler(ILGenerator gen, MethodBase original, IEnumerable<CodeInstruction> insns)
-        {
-            Log.trace("Patching Utility.canGrabSomethingFromHere");
-            return TileRadiusFix.IncreaseRadiusChecks(gen, original, insns);
-        }
-    }
-    [HarmonyPatch(typeof(Utility), "checkForCharacterInteractionAtTile")]
-    public class Utility_checkForCharacterInteractionAtTile
-    {
-        internal static IEnumerable<CodeInstruction> Transpiler(ILGenerator gen, MethodBase original, IEnumerable<CodeInstruction> insns)
-        {
-            Log.trace("Patching Utility.checkForCharacterInteractionAtTile");
-            return TileRadiusFix.IncreaseRadiusChecks(gen, original, insns);
-        }
-    }
-    [HarmonyPatch(typeof(Game1), "pressActionButton")]
-    public class Game1_pressActionButton
-    {
-        internal static IEnumerable<CodeInstruction> Transpiler(ILGenerator gen, MethodBase original, IEnumerable<CodeInstruction> insns)
-        {
-            Log.trace("Patching Game1.pressActionButton");
-            return TileRadiusFix.IncreaseRadiusChecks(gen, original, insns);
-        }
-    }
-    [HarmonyPatch(typeof(Game1), "pressUseToolButton")]
-    public class Game1_pressUseToolButton
-    {
-        internal static IEnumerable<CodeInstruction> Transpiler(ILGenerator gen, MethodBase original, IEnumerable<CodeInstruction> insns)
-        {
-            Log.trace("Patching Game1.pressUseToolButton");
-            return TileRadiusFix.IncreaseRadiusChecks(gen, original, insns);
-        }
-    }
-    [HarmonyPatch(typeof(Game1), "tryToCheckAt")]
-    public class Game1_tryToCheckAt
-    {
-        internal static IEnumerable<CodeInstruction> Transpiler(ILGenerator gen, MethodBase original, IEnumerable<CodeInstruction> insns)
-        {
-            Log.trace("Patching Game1.tryToCheckAt");
-            return TileRadiusFix.IncreaseRadiusChecks(gen, original, insns);
-        }
-    }
-    [HarmonyPatch(typeof(GameLocation), "isActionableTile")]
-    public class GameLocation_isActionableTile
-    {
-        internal static IEnumerable<CodeInstruction> Transpiler(ILGenerator gen, MethodBase original, IEnumerable<CodeInstruction> insns)
-        {
-            Log.trace("Patching GameLocation.isActionableTile");
-            return TileRadiusFix.IncreaseRadiusChecks(gen, original, insns);
         }
     }
 }
