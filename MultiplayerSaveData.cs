f920ee201658d8775d17f87de1ebff9a4ee84093
spacechase0 <spacechase0.and.cat@gmail.com>
2018-08-04 22:02:36 -0400

    Update manifest + forgotten file

---
+++ MultiplayerSaveData.cs
@@ -0,0 +1,13 @@
+﻿using StardewModdingAPI;
+using System.Collections.Generic;
+using System.IO;
+
+namespace CookingSkill
+{
+    public class MultiplayerSaveData
+    {
+        public static string FilePath => Path.Combine(Constants.CurrentSavePath, "cooking-skill-mp.json");
+
+        public Dictionary<long, int> Experience { get; set; } = new Dictionary<long, int>();
+    }
+}
