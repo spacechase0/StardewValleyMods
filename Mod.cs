538669f98186c608f8dcef9747a3f384eebf02f1
spacechase0 <spacechase0.and.cat@gmail.com>
2018-08-31 15:42:27 -0400

    Initial commit

---
+++ Mod.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewValley;
+
+namespace Spenny
+{
+    public class Mod : StardewModdingAPI.Mod
+    {
+        public static Mod instance;
+
+        public override void Entry(IModHelper helper)
+        {
+            instance = this;
+
+            GameEvents.EighthUpdateTick += test;
+        }
+
+        private void test(object sender, EventArgs args)
+        {
+            var penny = Game1.getCharacterFromName("Penny");
+            if (penny == null)
+                return;
+
+            penny.faceDirection((penny.FacingDirection + 1) % 4);
+            if ( penny.yJumpOffset == 0 )
+                penny.jump();
+        }
+    }
+}
