399dd6be2a9bd43b17c9b648b2f3c36a89813ee8
Jesse Plamondon-Willard <Pathoschild@users.noreply.github.com>
2018-12-22 14:19:12 -0500

    remove unused code

--- Mod.cs
+++ Mod.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using StardewModdingAPI;
+﻿using StardewModdingAPI;
 using StardewModdingAPI.Events;
 using Microsoft.Xna.Framework.Input;
 using StardewValley;
