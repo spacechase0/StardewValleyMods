428f1e991bafd970d9d2ef56d6e0b9b129f9dcb3
spacechase0 <spacechase0@users.noreply.github.com>
2019-01-29 10:25:14 -0500

    Initial commit

---
+++ Configuration.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnotherHungerMod
+{
+    public class Configuration
+    {
+        public int MaxFullness = 100;
+        public double EdibilityMultiplier = 1;
+        public double DrainPer10Min = 0.8;
+
+        public int PositiveBuffThreshold = 80;
+        public int NegativeBuffThreshold = 25;
+
+        public int StarvationDamagePer10Min = 10;
+
+        public int RelationshipHitForNotFeedingSpouse = 50;
+    }
+}
---
+++ Extensions.cs
@@ -0,0 +1,45 @@
+﻿using StardewModdingAPI;
+using StardewValley;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnotherHungerMod
+{
+    public static class Extensions
+    {
+        public static double GetFullness(this Farmer player)
+        {
+            if (player != Game1.player)
+                return -1;
+            return Mod.Data.Fullness;
+        }
+
+        public static void UseFullness(this Farmer player, double amt)
+        {
+            if (player != Game1.player)
+                return;
+
+            Mod.Data.Fullness = Math.Max(0, Math.Min(Mod.Data.Fullness - amt, player.GetMaxFullness()));
+            Mod.Data.SyncToHost();
+        }
+
+        public static int GetMaxFullness(this Farmer player)
+        {
+            return Mod.Config.MaxFullness;
+        }
+
+        public static bool HasFedSpouse(this Farmer player)
+        {
+            return Mod.Data.FedSpouseMeal;
+        }
+
+        public static void SetFedSpouse(this Farmer player, bool fed)
+        {
+            Mod.Data.FedSpouseMeal = fed;
+            Mod.Data.SyncToHost();
+        }
+    }
+}
