3f188ea367720d4199d91a33030398df1ea2281a
spacechase0 <spacechase0.and.cat@gmail.com>
2019-05-10 10:50:31 -0400

    Initial commit

---
+++ Configuration.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CombatLevelDamageScaler
+{
+    public class Configuration
+    {
+        public float DamageScalePerLevel = 0.05f;
+    }
+}
---
+++ Mod.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CombatLevelDamageScaler.Overrides;
+using Harmony;
+using Microsoft.Xna.Framework;
+using StardewModdingAPI;
+using StardewValley;
+
+namespace CombatLevelDamageScaler
+{
+    public class Mod : StardewModdingAPI.Mod
+    {
+        public static Configuration Config;
+
+        public override void Entry(IModHelper helper)
+        {
+            Config = helper.ReadConfig<Configuration>();
+
+            var harmony = HarmonyInstance.Create(ModManifest.UniqueID);
+            var target = AccessTools.Method(typeof(GameLocation), nameof(GameLocation.damageMonster),
+                                            new Type[] { typeof(Rectangle), typeof(int), typeof(int), typeof(bool), typeof(float), typeof(int), typeof(float), typeof(float), typeof(bool), typeof(Farmer) });
+            var patch = AccessTools.Method(typeof(DamageMonsterHook), nameof(DamageMonsterHook.Prefix));
+            Monitor.Log($"Patching {target} with {patch}");
+            harmony.Patch(target, prefix: new HarmonyMethod(patch));
+        }
+    }
+}
