47229acf8e03c64c4f5500eca20ad28090450f2d
spacechase0 <spacechase0.and.cat@gmail.com>
2019-05-14 21:17:31 -0400

    API fixes

--- Api.cs
+++ Api.cs
@@ -41,61 +41,85 @@ namespace JsonAssets
 
         public int GetObjectId(string name)
         {
+            if (Mod.instance.objectIds == null)
+                return -1;
             return Mod.instance.objectIds.ContainsKey(name) ? Mod.instance.objectIds[name] : -1;
         }
 
         public int GetCropId(string name)
         {
+            if (Mod.instance.cropIds == null)
+                return -1;
             return Mod.instance.cropIds.ContainsKey(name) ? Mod.instance.cropIds[name] : -1;
         }
 
         public int GetFruitTreeId(string name)
         {
+            if (Mod.instance.fruitTreeIds == null)
+                return -1;
             return Mod.instance.fruitTreeIds.ContainsKey(name) ? Mod.instance.fruitTreeIds[name] : -1;
         }
 
         public int GetBigCraftableId(string name)
         {
+            if (Mod.instance.bigCraftableIds == null)
+                return -1;
             return Mod.instance.bigCraftableIds.ContainsKey(name) ? Mod.instance.bigCraftableIds[name] : -1;
         }
 
         public int GetHatId(string name)
         {
+            if (Mod.instance.hatIds == null)
+                return -1;
             return Mod.instance.hatIds.ContainsKey(name) ? Mod.instance.hatIds[name] : -1;
         }
 
         public int GetWeaponId(string name)
         {
+            if (Mod.instance.weaponIds == null)
+                return -1;
             return Mod.instance.weaponIds.ContainsKey(name) ? Mod.instance.weaponIds[name] : -1;
         }
 
         public IDictionary<string, int> GetAllObjectIds()
         {
+            if (Mod.instance.objectIds == null)
+                return new Dictionary<string, int>();
             return new Dictionary<string, int>(Mod.instance.objectIds);
         }
 
         public IDictionary<string, int> GetAllCropIds()
         {
+            if (Mod.instance.cropIds == null)
+                return new Dictionary<string, int>();
             return new Dictionary<string, int>(Mod.instance.cropIds);
         }
 
         public IDictionary<string, int> GetAllFruitTreeIds()
         {
+            if (Mod.instance.fruitTreeIds == null)
+                return new Dictionary<string, int>();
             return new Dictionary<string, int>(Mod.instance.fruitTreeIds);
         }
 
         public IDictionary<string, int> GetAllBigCraftableIds()
         {
+            if (Mod.instance.bigCraftableIds == null)
+                return new Dictionary<string, int>();
             return new Dictionary<string, int>(Mod.instance.bigCraftableIds);
         }
 
         public IDictionary<string, int> GetAllHatIds()
         {
+            if (Mod.instance.hatIds == null)
+                return new Dictionary<string, int>();
             return new Dictionary<string, int>(Mod.instance.hatIds);
         }
 
         public IDictionary<string, int> GetAllWeaponIds()
         {
+            if (Mod.instance.weaponIds == null)
+                return new Dictionary<string, int>();
             return new Dictionary<string, int>(Mod.instance.weaponIds);
         }
 
