288edc6dd39782db5c18d5f690b16db55f12bbfa
spacechase0 <spacechase0.and.cat@gmail.com>
2019-05-18 15:04:46 -0400

    Forgot this file two commits ago

---
+++ Api.cs
@@ -0,0 +1,28 @@
+﻿using StardewValley;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceCore
+{
+    public interface IApi
+    {
+        string[] GetCustomSkills();
+        int GetLevelForCustomSkill(Farmer farmer, string skill);
+    }
+
+    public class Api : IApi
+    {
+        public string[] GetCustomSkills()
+        {
+            return Skills.GetSkillList();
+        }
+
+        public int GetLevelForCustomSkill(Farmer farmer, string skill)
+        {
+            return Skills.GetSkillLevel(farmer, skill);
+        }
+    }
+}
