e2a9bd32fc66f4acc11e9f8619907b4fbc6e1bb0
spacechase0 <spacechase0.and.cat@gmail.com>
2019-06-09 20:34:10 -0400

    Another fix I never committed (1.1.3)

--- Mod.cs
+++ Mod.cs
@@ -3,8 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Netcode;
 using StardewModdingAPI;
 using StardewValley;
+using xTile.ObjectModel;
 
 namespace CustomNPCFixes
 {
@@ -21,16 +24,49 @@ namespace CustomNPCFixes
             // This needs to be called again so that custom NPCs spawn in locations added after the original call
             Game1.fixProblems();
 
+            // Before we populate the route list, we need to fix doors from conditional CP patches and such.
+            // This can be removed once SMAPI 3.0 comes out.
+            foreach (var loc in Game1.locations)
+            {
+                loc.doors.Clear();
+                for (int x = 0; x < loc.map.Layers[0].LayerWidth; ++x)
+                {
+                    for (int y = 0; y < loc.map.Layers[0].LayerHeight; ++y)
+                    {
+                        if (loc.map.GetLayer("Buildings").Tiles[x, y] != null)
+                        {
+                            PropertyValue propertyValue3 = (PropertyValue)null;
+                            loc.map.GetLayer("Buildings").Tiles[x, y].Properties.TryGetValue("Action", out propertyValue3);
+                            if (propertyValue3 != null && propertyValue3.ToString().Contains("Warp"))
+                            {
+                                string[] strArray = propertyValue3.ToString().Split(' ');
+                                if (strArray[0].Equals("WarpCommunityCenter"))
+                                    loc.doors.Add(new Point(x, y), new NetString("CommunityCenter"));
+                                else if ((!loc.name.Equals((object)"Mountain") || x != 8 || y != 20) && strArray.Length > 2)
+                                    loc.doors.Add(new Point(x, y), new NetString(strArray[3]));
+                            }
+                        }
+                    }
+                }
+            }
+
             // Similarly, this needs to be called again so that pathing works.
             NPC.populateRoutesFromLocationToLocationList();
-            
+
             // Schedules for new NPCs don't work the first time.
             foreach ( var npc in Utility.getAllCharacters() )
             {
                 if ( npc.Schedule == null )
                 {
-                    npc.Schedule = npc.getSchedule(Game1.dayOfMonth);
-                    npc.checkSchedule(Game1.timeOfDay);
+                    try
+                    {
+                        npc.Schedule = npc.getSchedule(Game1.dayOfMonth);
+                        npc.checkSchedule(Game1.timeOfDay);
+                    }
+                    catch (Exception e)
+                    {
+                        Monitor.Log("Exception doing schedule for NPC " + npc.Name + ": " + e, LogLevel.Error);
+                    }
                 }
             }
         }
