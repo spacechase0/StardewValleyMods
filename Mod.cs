22b8b6027d324fd680f4c3e6897dcc77b85ac978
spacechase0 <spacechase0.and.cat@gmail.com>
2019-11-25 17:02:56 -0500

    1.1.4 - SDV 1.4

--- Mod.cs
+++ Mod.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Xna.Framework;
 using Netcode;
+using SpaceShared;
 using StardewModdingAPI;
 using StardewValley;
 using xTile.ObjectModel;
@@ -15,6 +16,8 @@ namespace CustomNPCFixes
     {
         public override void Entry(IModHelper helper)
         {
+            Log.Monitor = Monitor;
+
             helper.Events.GameLoop.SaveCreated += doNpcFixes;
             helper.Events.GameLoop.SaveLoaded += doNpcFixes;
         }
@@ -23,32 +26,6 @@ namespace CustomNPCFixes
         {
             // This needs to be called again so that custom NPCs spawn in locations added after the original call
             Game1.fixProblems();
-
-            // Before we populate the route list, we need to fix doors from conditional CP patches and such.
-            // This can be removed once SMAPI 3.0 comes out.
-            foreach (var loc in Game1.locations)
-            {
-                loc.doors.Clear();
-                for (int x = 0; x < loc.map.Layers[0].LayerWidth; ++x)
-                {
-                    for (int y = 0; y < loc.map.Layers[0].LayerHeight; ++y)
-                    {
-                        if (loc.map.GetLayer("Buildings").Tiles[x, y] != null)
-                        {
-                            PropertyValue propertyValue3 = (PropertyValue)null;
-                            loc.map.GetLayer("Buildings").Tiles[x, y].Properties.TryGetValue("Action", out propertyValue3);
-                            if (propertyValue3 != null && propertyValue3.ToString().Contains("Warp"))
-                            {
-                                string[] strArray = propertyValue3.ToString().Split(' ');
-                                if (strArray[0].Equals("WarpCommunityCenter"))
-                                    loc.doors.Add(new Point(x, y), new NetString("CommunityCenter"));
-                                else if ((!loc.name.Equals((object)"Mountain") || x != 8 || y != 20) && strArray.Length > 2)
-                                    loc.doors.Add(new Point(x, y), new NetString(strArray[3]));
-                            }
-                        }
-                    }
-                }
-            }
 
             // Similarly, this needs to be called again so that pathing works.
             NPC.populateRoutesFromLocationToLocationList();
@@ -65,7 +42,7 @@ namespace CustomNPCFixes
                     }
                     catch (Exception e)
                     {
-                        Monitor.Log("Exception doing schedule for NPC " + npc.Name + ": " + e, LogLevel.Error);
+                        Log.error("Exception doing schedule for NPC " + npc.Name + ": " + e);
                     }
                 }
             }
