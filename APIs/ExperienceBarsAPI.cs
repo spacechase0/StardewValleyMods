8cd83b88304e1ac457141c44dc4fe511d525750a
spacechase0 <spacechase0.and.cat@gmail.com>
2019-11-25 17:57:26 -0500

    Initial commit

---
+++ APIs/ExperienceBarsAPI.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+namespace SpaceShared.APIs
+{
+    public interface ExperienceBarsAPI
+    {
+        void DrawExperienceBar(Texture2D icon, int level, float percentFull, Color color);
+        void SetDrawLuck(bool luck);
+    }
+}
---
+++ APIs/JsonAssetsApi.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpaceShared.APIs
+{
+    public interface JsonAssetsAPI
+    {
+        void LoadAssets(string path);
+
+        int GetObjectId(string name);
+        int GetCropId(string name);
+        int GetFruitTreeId(string name);
+        int GetBigCraftableId(string name);
+        int GetHatId(string name);
+        int GetWeaponId(string name);
+        int GetClothingId(string name);
+
+        IDictionary<string, int> GetAllObjectIds();
+        IDictionary<string, int> GetAllCropIds();
+        IDictionary<string, int> GetAllFruitTreeIds();
+        IDictionary<string, int> GetAllBigCraftableIds();
+        IDictionary<string, int> GetAllHatIds();
+        IDictionary<string, int> GetAllWeaponIds();
+        IDictionary<string, int> GetAllClothingIds();
+
+        event EventHandler IdsAssigned;
+        event EventHandler AddedItemsToShop;
+
+        bool TryGetCustomSprite(object entity, out Texture2D texture, out Rectangle sourceRect);
+        bool TryGetCustomSpriteSheet(object entity, out Texture2D texture, out Rectangle sourceRect);
+    }
+}
