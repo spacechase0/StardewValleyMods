92b7fe7e0c20c9ecd7c9687b15e963c70bae56fa
spacechase0 <spacechase0.and.cat@gmail.com>
2019-11-25 17:58:41 -0500

    Initial commit

---
+++ Mod.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SpaceShared;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewValley;
+using StardewValley.Tools;
+
+namespace ThrowableAxe
+{
+    public class Mod : StardewModdingAPI.Mod
+    {
+        public static Mod instance;
+        private ThrownAxe thrown;
+        private bool clicking = false;
+
+        public override void Entry(IModHelper helper)
+        {
+            instance = this;
+            Log.Monitor = Monitor;
+
+            helper.Events.Input.ButtonPressed += onButtonPress;
+            helper.Events.Input.ButtonReleased += onButtonRelease;
+            helper.Events.GameLoop.UpdateTicking += onUpdateTicking;
+            helper.Events.Player.Warped += onWarped;
+        }
+
+        private void onButtonPress(object sender, ButtonPressedEventArgs e)
+        {
+            if (!Context.IsPlayerFree)
+                return;
+
+            if (e.Button == SButton.MouseRight && Game1.player.CurrentTool is Axe axe && thrown == null)
+            {
+                int[] dmg_ = new int[] { 8, 15, 30, 45, 60, 80 }; // 6 for support for prismatic tools
+                float[] speed_ = new float[] { 10, 12, 14, 16, 18, 20 }; // 6 for support for prismatic tools
+                int dmg = dmg_[axe.UpgradeLevel];
+                float speed = speed_[axe.UpgradeLevel];
+                
+                thrown = new ThrownAxe(Game1.player, axe.UpgradeLevel, dmg, e.Cursor.AbsolutePixels, speed);
+                Game1.currentLocation.projectiles.Add(thrown);
+
+                Log.trace("Throwing axe");
+                clicking = true;
+            }
+        }
+
+        private void onButtonRelease(object sender, ButtonReleasedEventArgs e)
+        {
+            if (e.Button == SButton.MouseRight)
+            {
+                clicking = false;
+            }
+        }
+
+        private void onUpdateTicking(object sender, UpdateTickingEventArgs e)
+        {
+            if (thrown != null)
+            {
+                /*
+                if(clicking)
+                {
+                    thrown.target.Value = Helper.Input.GetCursorPosition().AbsolutePixels;
+                }
+                */
+                if (!clicking || (thrown.GetPosition() - thrown.target).Length() < 1)
+                {
+                    var playerPos = Game1.player.getStandingPosition();
+                    playerPos.X -= 16;
+                    playerPos.Y -= 64;
+                    thrown.target.Value = playerPos;
+                    if ( (thrown.GetPosition() - playerPos).Length() < 16 )
+                    {
+                        thrown.dead = true;
+                    }
+                }
+
+                if (thrown.dead)
+                {
+                    Log.trace("Axe destroyed");
+                    thrown = null;
+                }
+            }
+        }
+
+        private void onWarped(object sender, WarpedEventArgs e)
+        {
+            if (thrown != null)
+            {
+                thrown.dead = true;
+                thrown = null;
+            }
+        }
+    }
+
+}
---
+++ ThrownAxe.cs
@@ -0,0 +1,118 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Netcode;
+using StardewValley;
+using StardewValley.Monsters;
+using StardewValley.Projectiles;
+using StardewValley.TerrainFeatures;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThrowableAxe
+{
+    public class ThrownAxe : Projectile
+    {
+        private GameLocation loc;
+        private readonly NetInt tier = new NetInt(0);
+        private readonly NetInt damage = new NetInt(3);
+        public readonly NetVector2 target = new NetVector2();
+        private readonly NetFloat speed = new NetFloat(1);
+        private float axeRot = 0;
+        public bool dead = false;
+        public List<NPC> npcsHit = new List<NPC>();
+
+        // Do I need this for it to be net-okay?
+        public ThrownAxe()
+        {
+        }
+
+        public ThrownAxe(Farmer thrower, int tier, int damage, Vector2 target, float speed)
+        {
+            this.position.X = thrower.getStandingX() - 16;
+            this.position.Y = thrower.getStandingY() - 64;
+
+            this.loc = thrower.currentLocation;
+            this.theOneWhoFiredMe.Set(this.loc, thrower);
+            this.damagesMonsters.Value = true;
+            this.tier.Value = tier;
+            this.damage.Value = damage;
+            this.target.Value = target;
+            this.speed.Value = speed;
+            boundingBoxWidth = 64;
+            boundingBoxHeight = 64;
+            this.NetFields.AddFields(this.tier, this.damage, this.target, this.speed);
+        }
+
+        public override void behaviorOnCollisionWithMineWall(int tileX, int tileY)
+        {
+        }
+
+        public override void behaviorOnCollisionWithMonster(NPC n, GameLocation location)
+        {
+            if (npcsHit.Contains(n))
+                return;
+
+            npcsHit.Add(n);
+            if (n is Monster mob)
+            {
+                location.damageMonster(getBoundingBox(), damage, damage, false, (Farmer)theOneWhoFiredMe.Get(location));
+            }
+        }
+
+        public override void behaviorOnCollisionWithOther(GameLocation location)
+        {
+        }
+
+        public override void behaviorOnCollisionWithPlayer(GameLocation location, Farmer player)
+        {
+        }
+
+        public override void behaviorOnCollisionWithTerrainFeature(TerrainFeature t, Vector2 tileLocation, GameLocation location)
+        {
+        }
+
+        public override bool update(GameTime time, GameLocation location)
+        {
+            base.update(time, location);
+            return dead;
+        }
+
+        public override void updatePosition(GameTime time)
+        {
+            Vector2 targetDiff = target.Value - position.Value;
+            Vector2 targetDir = targetDiff;
+            targetDir.Normalize();
+
+            if (targetDiff.Length() < speed.Value)
+                position.Value = target.Value;
+            else
+                position.Value += targetDir * speed.Value;
+
+            //Log.trace($"{position.Value} {target.Value} {targetDir}");
+        }
+
+        public override void draw(SpriteBatch b)
+        {
+            int sheetShift = tier * 7;
+            if (tier > 2)
+                sheetShift += 21;
+            var sourceRect = Game1.getSquareSourceRectForNonStandardTileSheet(Game1.toolSpriteSheet, 16, 16, 215 + sheetShift);
+            //b.Draw(Game1.staminaRect, Game1.GlobalToLocal(Game1.viewport, getBoundingBox()), null, Color.Red, 0, Vector2.Zero, SpriteEffects.None, 0.99f);
+            b.Draw(Game1.toolSpriteSheet, Game1.GlobalToLocal(Game1.viewport, position + new Vector2(32, 32)), sourceRect, Color.White, rotation, new Vector2(8, 8), 4, SpriteEffects.None, 1);
+            rotation += 0.3f;
+        }
+
+        public override Rectangle getBoundingBox()
+        {
+            return base.getBoundingBox();
+        }
+
+        public Vector2 GetPosition()
+        {
+            return position.Value;
+        }
+    }
+}
