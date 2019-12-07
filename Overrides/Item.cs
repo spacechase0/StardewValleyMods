44eaea9000fc2c4020c3330c037957ff879f1319
Chase W <spacechase0@users.noreply.github.com>
2019-12-07 18:04:31 -0500

    Forgot to include these

---
+++ Overrides/Item.cs
@@ -0,0 +1,31 @@
+﻿using StardewValley;
+using StardewValley.Objects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarryChest.Overrides
+{
+    public static class ItemCanStackHook
+    {
+        public static bool Prefix(Item __instance, ISalable other, ref bool __result )
+        {
+            // We're checking the `.ParentSheetIndex` instead of `is Chest` because when you break a chest 
+            // and pick it up it isn't a chest instance, it's just an object with the chest index.
+            if ( __instance.ParentSheetIndex== 130 && (other is StardewValley.Object obj && obj.ParentSheetIndex == 130) )
+            {
+                Chest c1 = __instance as Chest;
+                Chest c2 = other as Chest;
+                if ( c1 != null && c1.items.Count != 0 || c2 != null && c2.items.Count != 0 )
+                {
+                    __result = false;
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ Overrides/Object.cs
@@ -0,0 +1,22 @@
+﻿using StardewValley;
+using StardewValley.Objects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarryChest.Overrides
+{
+    public static class ObjectDescriptionHook
+    {
+        public static void Postfix(StardewValley.Object __instance, ref string __result)
+        {
+            if ( __instance.ParentSheetIndex == 130 )
+            {
+                var chest = __instance as Chest;
+                __result += "\n" + $"Contains {chest?.items?.Count ?? 0} items.";
+            }
+        }
+    }
+}
