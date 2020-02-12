77fc8ba19fbd8fc0e15276b408e30fcbeb4ffac2
Elec0 <elec08215@gmail.com>
2020-02-11 20:35:42 -0800

    Remove overhealing with heal spell

--- Spells/HealSpell.cs
+++ Spells/HealSpell.cs
@@ -24,6 +24,8 @@ namespace Magic.Spells
         public override IActiveEffect onCast(Farmer player, int level, int targetX, int targetY)
         {
             int health = 10 + 15 * level + (player.CombatLevel + 1) * 2;
+            if (player.health + health >= player.maxHealth)
+                health = player.maxHealth - player.health;
             player.health += health;
             player.currentLocation.debris.Add(new Debris(health, new Vector2((float)(Game1.player.getStandingX() + 8), (float)Game1.player.getStandingY()), Color.Green, 1f, (Character)Game1.player));
             Game1.playSound("healSound");
