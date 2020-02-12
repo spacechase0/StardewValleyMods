c4d8e861194137bb2db453e26a959071f4b16781
Elec0 <elec08215@gmail.com>
2020-02-12 03:09:42 -0800

    Un-stupid a bug fix

--- Spells/HealSpell.cs
+++ Spells/HealSpell.cs
@@ -25,7 +25,7 @@ namespace Magic.Spells
         {
             int health = 10 + 15 * level + (player.CombatLevel + 1) * 2;
             if (player.health + health >= player.maxHealth)
-                health = player.maxHealth - player.health;
+                health = player.maxHealth;
             player.health += health;
             player.currentLocation.debris.Add(new Debris(health, new Vector2((float)(Game1.player.getStandingX() + 8), (float)Game1.player.getStandingY()), Color.Green, 1f, (Character)Game1.player));
             Game1.playSound("healSound");
