c1165335c88c939c941c3e5208056890ecd5ca83
Jesse Plamondon-Willard <Pathoschild@users.noreply.github.com>
2020-04-25 22:08:45 -0400

    update & standardize project structure
    This commit...
    - adds common.targets for shared build settings;
    - updates the mod build package;
    - updates the target framework;
    - migrates to the new .csproj format;
    - targets x86 to avoid mismatched processor architecture warnings.

--- Properties/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyTitle("ThreeHeartDancePartner")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyVersion("1.0.3.0")]
-[assembly: AssemblyFileVersion("1.0.3.0")]
