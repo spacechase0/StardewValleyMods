1e1fc90e3c220e51117baacf5a7eb98d6e06d4d8
Chase W <spacechase0@users.noreply.github.com>
2020-06-17 13:06:35 -0400

    Initial commit

---
+++ Configuration.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JunimosAcceptCash
+{
+    public class Configuration
+    {
+        public int CostMultiplier { get; set; } = 10;
+    }
+}
