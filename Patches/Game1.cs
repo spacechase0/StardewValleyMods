f4c92b4bc36a499bd634be6848295b8c9d931ed9
Chase W <spacechase0@users.noreply.github.com>
2020-08-16 21:51:21 -0400

    Inital commit

---
+++ Patches/Game1.cs
@@ -0,0 +1,12 @@
+﻿using Harmony;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocationLayerTool.Patches
+{
+    //[HarmonyPatch( typeof( Game1 ), "_draw" )]
+    
+}
