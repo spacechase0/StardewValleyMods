13f31cf88ab3e0a0c8860808e39053250fc0acdf
Chase W <spacechase0@users.noreply.github.com>
2020-12-24 22:29:55 -0500

    Initial commit

---
+++ Enchantments/MoreLuresEnchantment.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using StardewValley;
+using StardewValley.Tools;
+
+namespace MoreEnchantments.Enchantments
+{
+    [XmlType("Mods_MoreLuresEnchantment")]
+    public class MoreLuresEnchantment : FishingRodEnchantment
+    {
+        public override string GetName()
+        {
+            return "A-lure-ing";
+        }
+
+        protected override void _ApplyTo( Item item )
+        {
+            base._ApplyTo( item );
+            ( item as FishingRod ).numAttachmentSlots.Value = 3;
+            ( item as FishingRod ).attachments.SetCount( 3 );
+        }
+
+        protected override void _UnapplyTo( Item item )
+        {
+            base._UnapplyTo( item );
+            ( item as FishingRod ).numAttachmentSlots.Value = 2;
+            ( item as FishingRod ).attachments.SetCount( 2 );
+        }
+    }
+}
---
+++ Mod.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Harmony;
+using MoreEnchantments.Enchantments;
+using SpaceShared;
+using StardewModdingAPI;
+using StardewValley;
+
+namespace MoreEnchantments
+{
+    public class Mod : StardewModdingAPI.Mod, IAssetEditor
+    {
+        public static Mod instance;
+
+        public bool CanEdit<T>( IAssetInfo asset )
+        {
+            return asset.AssetNameEquals( "Strings\\EnchantmentNames" );
+        }
+
+        public void Edit<T>( IAssetData asset )
+        {
+            asset.AsDictionary<string, string>().Data.Add( "MoreLures", "A-lure-ing" );
+        }
+
+        public override void Entry( IModHelper helper )
+        {
+            instance = this;
+            Log.Monitor = Monitor;
+
+            BaseEnchantment.GetAvailableEnchantments().Add( new MoreLuresEnchantment() );
+
+            var harmony = HarmonyInstance.Create( ModManifest.UniqueID );
+            harmony.PatchAll();
+        }
+    }
+}
