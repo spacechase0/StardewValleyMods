1687abf99ee1b6547c05c32ca06edf0a895f75db
Chase W <spacechase0@users.noreply.github.com>
2020-12-24 22:00:29 -0500

    Initial commit

---
+++ ContentList.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BiggerCraftables
+{
+    public class ContentList
+    {
+        public class Entry
+        {
+            [JsonIgnore]
+            public Texture2D Texture { get; set; }
+
+            public string Name { get; set; }
+            public string Image { get; set; }
+            public int Width { get; set; }
+            public int Length { get; set; }
+        }
+
+        public List<Entry> BiggerCraftables { get; set; } = new List<Entry>();
+    }
+}
