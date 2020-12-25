fc41df4c819a23582bffe0f6fb86d63c1f1216d7
Chase W <spacechase0@users.noreply.github.com>
2020-12-24 22:04:25 -0500

    Initial commit

---
+++ Patches/GameLocation.cs
@@ -0,0 +1,55 @@
+﻿using Harmony;
+using SpaceShared;
+using StardewValley;
+using StardewValley.Locations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuildableLocationsFramework.Patches
+{
+    [HarmonyPatch(typeof(GameLocation), "carpenters")]
+    public static class GameLocationCarpentersPatch
+    {
+        public static IEnumerable<CodeInstruction> Transpiler( ILGenerator gen, MethodBase original, IEnumerable<CodeInstruction> insns )
+        {
+            Log.info( "Transpiling " + original );
+            List<CodeInstruction> ret = new List<CodeInstruction>();
+
+            foreach ( var insn in insns )
+            {
+                if ( insn.opcode == OpCodes.Callvirt && insn.operand is MethodInfo info )
+                {
+                    if ( info.DeclaringType == typeof( BuildableGameLocation ) && info.Name == "isThereABuildingUnderConstruction" )
+                    {
+                        Log.debug( "Found isThereABuildingUnderConstruction, replacing..." );
+                        var newInsn = new CodeInstruction( OpCodes.Call, typeof( GameLocationCarpentersPatch ).GetMethod( nameof( IsAnyBuildingUnderConstruction ) ) );
+                        ret.Add( newInsn );
+                        continue;
+                    }
+                }
+                ret.Add( insn );
+            }
+
+            return ret;
+        }
+
+        public static bool IsAnyBuildingUnderConstruction( Farm originalParam )
+        {
+            foreach ( var loc in Mod.GetAllLocations() )
+            {
+                if ( loc is BuildableGameLocation bgl )
+                {
+                    if ( bgl.isThereABuildingUnderConstruction() )
+                        return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
---
+++ Patches/SaveGame.cs
@@ -0,0 +1,48 @@
+﻿using Harmony;
+using Netcode;
+using StardewValley;
+using StardewValley.Buildings;
+using StardewValley.Locations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuildableLocationsFramework.Patches
+{
+    [HarmonyPatch(typeof(SaveGame), nameof(SaveGame.loadDataToLocations))]
+    public static class SaveGameLoadDataToLocationsPatch
+    {
+        internal static List<GameLocation> locs;
+
+        public static void Prefix( List<GameLocation> gamelocations )
+        {
+            locs = gamelocations;
+
+            foreach ( GameLocation gamelocation in gamelocations )
+            {
+                if ( gamelocation.Name == "Farm" )
+                    continue;
+                if ( gamelocation is BuildableGameLocation bgl )
+                {
+                    GameLocation locationFromName = Game1.getLocationFromName((string)(NetFieldBase<string, NetString>)gamelocation.name);
+                    foreach ( Building building in ( ( BuildableGameLocation ) gamelocation ).buildings )
+                        building.load();
+                    ( ( BuildableGameLocation ) locationFromName ).buildings.Set( ( ICollection<Building> ) ( ( BuildableGameLocation ) gamelocation ).buildings );
+                }
+                else if ( gamelocation is IAnimalLocation al )
+                {
+                    foreach ( FarmAnimal farmAnimal in al.Animals.Values )
+                        farmAnimal.reload( ( Building ) null );
+                }
+            }
+
+        }
+
+        public static void Postfix()
+        {
+            locs = null;
+        }
+    }
+}
