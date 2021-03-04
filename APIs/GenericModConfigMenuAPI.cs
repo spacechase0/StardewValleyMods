f044f715785611a5acf3a1f5523cab807eec115d
Chase Warrington <spacechase0.and.cat@gmail.com>
2021-03-03 20:54:29 -0500

    Update GMCM API

--- APIs/GenericModConfigMenuAPI.cs
+++ APIs/GenericModConfigMenuAPI.cs
@@ -7,23 +7,38 @@ namespace SpaceShared.APIs
 {
     public interface GenericModConfigMenuAPI
     {
-        void RegisterModConfig(IManifest mod, Action revertToDefault, Action saveToFile);
+        void RegisterModConfig( IManifest mod, Action revertToDefault, Action saveToFile );
 
-        void RegisterLabel(IManifest mod, string labelName, string labelDesc);
-        void RegisterSimpleOption(IManifest mod, string optionName, string optionDesc, Func< bool > optionGet, Action< bool > optionSet);
-        void RegisterSimpleOption(IManifest mod, string optionName, string optionDesc, Func< int > optionGet, Action< int > optionSet);
-        void RegisterSimpleOption(IManifest mod, string optionName, string optionDesc, Func< float > optionGet, Action< float > optionSet);
-        void RegisterSimpleOption(IManifest mod, string optionName, string optionDesc, Func< string > optionGet, Action< string > optionSet);
-        void RegisterSimpleOption(IManifest mod, string optionName, string optionDesc, Func< SButton > optionGet, Action< SButton > optionSet);
+        void StartNewPage( IManifest mod, string pageName );
 
-        void RegisterClampedOption(IManifest mod, string optionName, string optionDesc, Func< int > optionGet, Action< int > optionSet, int min, int max);
-        void RegisterClampedOption(IManifest mod, string optionName, string optionDesc, Func< float > optionGet, Action<float> optionSet, float min, float max);
+        void RegisterLabel( IManifest mod, string labelName, string labelDesc );
+        void RegisterPageLabel( IManifest mod, string labelName, string labelDesc, string newPage );
+        void RegisterParagraph( IManifest mod, string paragraph );
+        void RegisterImage( IManifest mod, string texPath, Rectangle? texRect = null, int scale = 4 );
 
-        void RegisterChoiceOption(IManifest mod, string optionName, string optionDesc, Func< string > optionGet, Action< string > optionSet, string[] choices);
+        void RegisterSimpleOption( IManifest mod, string optionName, string optionDesc, Func<bool> optionGet, Action<bool> optionSet );
+        void RegisterSimpleOption( IManifest mod, string optionName, string optionDesc, Func<int> optionGet, Action<int> optionSet );
+        void RegisterSimpleOption( IManifest mod, string optionName, string optionDesc, Func<float> optionGet, Action<float> optionSet );
+        void RegisterSimpleOption( IManifest mod, string optionName, string optionDesc, Func<string> optionGet, Action<string> optionSet );
+        void RegisterSimpleOption( IManifest mod, string optionName, string optionDesc, Func<SButton> optionGet, Action<SButton> optionSet );
 
-        void RegisterComplexOption(IManifest mod, string optionName, string optionDesc,
-                                   Func< Vector2, object, object > widgetUpdate,
-                                   Func< SpriteBatch, Vector2, object, object > widgetDraw,
-                                   Action< object > onSave);
+        void RegisterClampedOption( IManifest mod, string optionName, string optionDesc, Func<int> optionGet, Action<int> optionSet, int min, int max );
+        void RegisterClampedOption( IManifest mod, string optionName, string optionDesc, Func<float> optionGet, Action<float> optionSet, float min, float max );
+        void RegisterClampedOption( IManifest mod, string optionName, string optionDesc, Func<int> optionGet, Action<int> optionSet, int min, int max, int interval );
+        void RegisterClampedOption( IManifest mod, string optionName, string optionDesc, Func<float> optionGet, Action<float> optionSet, float min, float max, float interval );
+
+        void RegisterChoiceOption( IManifest mod, string optionName, string optionDesc, Func<string> optionGet, Action<string> optionSet, string[] choices );
+
+        void RegisterComplexOption( IManifest mod, string optionName, string optionDesc,
+                                   Func<Vector2, object, object> widgetUpdate,
+                                   Func<SpriteBatch, Vector2, object, object> widgetDraw,
+                                   Action<object> onSave );
+
+        void SubscribeToChange( IManifest mod, Action<string, bool> changeHandler );
+        void SubscribeToChange( IManifest mod, Action<string, int> changeHandler );
+        void SubscribeToChange( IManifest mod, Action<string, float> changeHandler );
+        void SubscribeToChange( IManifest mod, Action<string, string> changeHandler );
+
+        void OpenModMenu( IManifest mod );
     }
 }
