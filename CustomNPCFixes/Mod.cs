a74c7256638bf1cd9a421050d5eba888097ffe45
Jesse Plamondon-Willard <Pathoschild@users.noreply.github.com>
2021-06-03 17:25:04 -0400

    remove unused Harmony references

--- CustomNPCFixes/Mod.cs
+++ CustomNPCFixes/Mod.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Harmony;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Netcode;
