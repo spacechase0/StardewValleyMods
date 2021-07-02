1bbb565f020688a7ad4479f37b2c973469b332ed
lshtech <elbebeta@gmail.com>
2021-07-01 21:38:14 -0500

    Revert "Getting projects in stable state, updated BFAV to support ostriches"
    This reverts commit bcc67e9a2d8fc2a497edff509a3eff2bb13d718a.

--- SkillPrestige.CookingSkill/Properties/AssemblyInfo.cs
+++
@@ -1,5 +0,0 @@
-using System.Reflection;
-
-[assembly: AssemblyTitle("SkillPrestige.CookingSkill")]
-[assembly: AssemblyVersion("2.4.0")]
-[assembly: AssemblyFileVersion("2.4.0")]
--- SkillPrestige/Properties/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-
-[assembly: AssemblyTitle("SkillPrestige")]
-[assembly: InternalsVisibleTo("UnitTest")]
-[assembly: AssemblyVersion("2.4.0")]
-[assembly: AssemblyFileVersion("2.4.0")]
