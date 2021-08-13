9b3bc6c2a3637ec8ee10d1bf77ace54344d4cdeb
Jesse Plamondon-Willard <Pathoschild@users.noreply.github.com>
2021-08-13 00:45:53 -0400

    update for accessibility change in Stardew Valley 1.5.5

--- CustomizeExterior/Mod.cs
+++ CustomizeExterior/Mod.cs
@@ -328,7 +328,7 @@ namespace CustomizeExterior
                 buildingType: "houses",
                 folderName: farm.GetFarmhouseAssetPackName(),
                 debugLabel: "farmhouse",
-                update: texture => typeof(Farm).GetField(nameof(Farm.houseTextures)).SetValue(null, texture)
+                update: texture => Farm.houseTextures = texture
             );
         }
 
