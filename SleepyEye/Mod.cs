12bcee2212cebeda99812f34101912757a17af76
Jesse Plamondon-Willard <Pathoschild@users.noreply.github.com>
2021-10-11 23:54:25 -0400

    add translation support for Sleepy Eye

--- SleepyEye/Mod.cs
+++ SleepyEye/Mod.cs
@@ -38,6 +38,7 @@ namespace SleepyEye
             this.ApplyConfig(helper.ReadConfig<ModConfig>());
 
             // init
+            I18n.Init(helper.Translation);
             Mod.Instance = this;
             Log.Monitor = this.Monitor;
             helper.Events.Display.MenuChanged += this.OnMenuChanged;
@@ -83,8 +84,8 @@ namespace SleepyEye
                 );
                 configMenu.AddNumberOption(
                     mod: this.ModManifest,
-                    name: () => "Seconds until save",
-                    tooltip: () => "The number of seconds until the tent tool should trigger a save.",
+                    name: I18n.Config_SecondsUntilSave_Name,
+                    tooltip: I18n.Config_SecondsUntilSave_Tooltip,
                     getValue: () => (int)TentTool.UseDelay.TotalSeconds,
                     setValue: value => TentTool.UseDelay = TimeSpan.FromSeconds(value)
                 );
@@ -144,12 +145,12 @@ namespace SleepyEye
             GameLocation location = Game1.getLocationFromName(camp.Location);
             if (location == null)
             {
-                Game1.addHUDMessage(new HUDMessage("You camped somewhere strange, so you've returned home."));
+                Game1.addHUDMessage(new HUDMessage(I18n.Messages_SleptAtLostLocation()));
                 return;
             }
             if (location.Name == this.GetFestivalLocation())
             {
-                Game1.addHUDMessage(new HUDMessage("You camped on the festival grounds, so you've returned home."));
+                Game1.addHUDMessage(new HUDMessage(I18n.Messages_SleptAtFestival()));
                 return;
             }
 
--- SleepyEye/TentTool.cs
+++ SleepyEye/TentTool.cs
@@ -2,6 +2,7 @@ using System;
 using System.Xml.Serialization;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using SleepyEye.Framework;
 using StardewModdingAPI;
 using StardewValley;
 
@@ -169,13 +170,13 @@ namespace SleepyEye
         /// <inheritdoc />
         protected override string loadDescription()
         {
-            return "Sleep here. Sleep there. Sleep everywhere!";
+            return I18n.Tent_Description();
         }
 
         /// <inheritdoc />
         protected override string loadDisplayName()
         {
-            return "Tent";
+            return I18n.Tent_Name();
         }
 
         /// <inheritdoc />
