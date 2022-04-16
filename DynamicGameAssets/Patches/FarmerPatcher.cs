98f3553a03bd99536c526c6e775bd18583e22fd4
atravita-mods <94934860+atravita-mods@users.noreply.github.com>
2022-04-16 17:26:19 -0400

    Adds back in the TAS for eating animations.

--- DynamicGameAssets/Patches/FarmerPatcher.cs
+++ DynamicGameAssets/Patches/FarmerPatcher.cs
@@ -5,9 +5,11 @@ using System.Reflection.Emit;
 using DynamicGameAssets.Framework;
 using DynamicGameAssets.Game;
 using HarmonyLib;
+using Microsoft.Xna.Framework;
 using Spacechase.Shared.Patching;
 using SpaceShared;
 using StardewModdingAPI;
+using StardewModdingAPI.Utilities;
 using StardewValley;
 
 namespace DynamicGameAssets.Patches
@@ -16,6 +18,7 @@ namespace DynamicGameAssets.Patches
     [SuppressMessage("ReSharper", "InconsistentNaming", Justification = DiagnosticMessages.NamedForHarmony)]
     internal class FarmerPatcher : BasePatcher
     {
+
         /*********
         ** Public methods
         *********/
@@ -37,6 +40,10 @@ namespace DynamicGameAssets.Patches
             harmony.Patch(
                 original: this.RequireMethod<Farmer>(nameof(Farmer.doneEating)),
                 transpiler: this.GetHarmonyMethod(nameof(Transpile_DoneEating))
+            );
+            harmony.Patch(
+                original: this.RequireMethod<Farmer>(nameof(Farmer.showEatingItem)),
+                postfix: this.GetHarmonyMethod(nameof(Postfix_showEatingItem))
             );
         }
 
@@ -117,6 +124,110 @@ namespace DynamicGameAssets.Patches
         private static IEnumerable<CodeInstruction> Transpile_DoneEating(ILGenerator gen, MethodBase original, IEnumerable<CodeInstruction> instructions)
         {
             return PatchCommon.RedirectForFakeObjectInformationTranspiler2(gen, original, instructions);
+        }
+
+        /// <summary>
+        /// Postfixes <see cref="Farmer.showEatingItem(Farmer)" /> to add back in the TAS for DGA.
+        /// </summary>
+        /// <param name="who">Farmer.</param>
+        private static void Postfix_showEatingItem(Farmer who)
+        {
+            if (who.itemToEat is CustomObject obj)
+            {
+                PackData.TextureAnimationFrame animationFrame = obj.Data.pack.GetTextureFrame(obj.Data.Texture);
+                TexturedRect tex = obj.Data.pack.GetTexture(obj.Data.Texture, 16, 16);
+                string textureName = obj.Data.pack.smapiPack.GetActualAssetKey(animationFrame.FilePath);
+
+                if (tex.Rect is null)
+                {
+                    return;
+                }
+
+                switch (who.FarmerSprite.currentAnimationIndex)
+                {
+                    case 1:
+                    {
+                        TemporaryAnimatedSprite tas = new(
+                            textureName: textureName,
+                            sourceRect: tex.Rect.Value,
+                            animationInterval: 254f,
+                            animationLength: 1,
+                            numberOfLoops: 0,
+                            position: who.Position + new Vector2(-21f, -112f),
+                            flicker: false,
+                            flipped: false,
+                            layerDepth: who.getStandingY() / 10000f + 0.01f,
+                            alphaFade: 0f,
+                            color: Color.White,
+                            scale: Game1.pixelZoom,
+                            scaleChange: 0f,
+                            rotation: 0f,
+                            rotationChange: 0f);
+                        who.currentLocation.temporarySprites.Add(tas);
+                        return;
+                    }
+                    case 2:
+                    {
+                        TemporaryAnimatedSprite tas = new(
+                            textureName: textureName,
+                            sourceRect: tex.Rect.Value,
+                            animationInterval: 650f,
+                            animationLength: 1,
+                            numberOfLoops: 0,
+                            position: who.Position + new Vector2(-21f, -108f),
+                            flicker: false,
+                            flipped: false,
+                            layerDepth: who.getStandingY() / 10000f + 0.01f,
+                            alphaFade: 0f,
+                            color: Color.White,
+                            scale: Game1.pixelZoom,
+                            scaleChange: -0.01f,
+                            rotation: 0f,
+                            rotationChange: 0f)
+                        {
+                            motion = new Vector2(0.8f, -11f),
+                            acceleration = new Vector2(0f, 0.5f)
+                        };
+                        who.currentLocation.temporarySprites.Add(tas);
+                        return;
+                    }
+                    case 4:
+                    {
+                        for (int i = 0; i < 8; i++)
+                        {
+                            Rectangle r = tex.Rect.Value;
+                            r.X += 8;
+                            r.Y += 8;
+                            r.Width = 4;
+                            r.Height = 4;
+                            TemporaryAnimatedSprite tas = new(
+                                textureName: textureName,
+                                sourceRect: r,
+                                animationInterval: 400f,
+                                animationLength: 1,
+                                numberOfLoops: 0,
+                                position: who.Position + new Vector2(24f, -48f),
+                                flicker: false,
+                                flipped: false,
+                                layerDepth: who.getStandingY() / 10000f + 0.01f,
+                                alphaFade: 0f,
+                                color: Color.White,
+                                scale: Game1.pixelZoom,
+                                scaleChange: 0f,
+                                rotation: 0f,
+                                rotationChange: 0f)
+                            {
+                                motion = new Vector2((float)Game1.random.Next(-30, 31) / 10f, Game1.random.Next(-6, -3)),
+                                acceleration = new Vector2(0f, 0.5f)
+                            };
+                            who.currentLocation.temporarySprites.Add(tas);
+                        }
+                        return;
+                    }
+                    default:
+                        return;
+                }
+            }
         }
     }
 }
