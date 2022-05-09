e969dbaa8a867188ebb038bbb8495dab9c0fbf6e
NormanPCN <91967509+NormanPCN@users.noreply.github.com>
2022-05-09 12:43:56 -0700

    Chests Anywhere compat, purchase dialog, use new SMAPI 3.14/4 Api
    Use new Chests Anywhere Api for suppress mouse clicks while ChestsAny options dialog is active.
    
    Add new dialog, Alt+RightClick, to open a dialog to enter a purchase quantity.
    
    Change to new SMAPI 3.14/4.0 Apis for content loading. Now requires SMAPI 3.14+.

--- BetterShopMenu/Framework/Configuration.cs
+++ BetterShopMenu/Framework/Configuration.cs
@@ -3,5 +3,6 @@ namespace BetterShopMenu.Framework
     internal class Configuration
     {
         public bool GridLayout { get; set; } = false;
+        public bool QuantityDialog { get; set; } = false;
     }
 }
