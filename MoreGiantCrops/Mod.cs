a5b2370b82d6a30f544525166677631a45f38267
atravita-mods <94934860+atravita-mods@users.noreply.github.com>
2022-10-02 13:17:16 -0400

    Load from content packs because I guess why not.

--- MoreGiantCrops/Mod.cs
+++ MoreGiantCrops/Mod.cs
@@ -13,6 +13,8 @@ using SpaceShared;
 
 using StardewModdingAPI;
 
+using StardewValley;
+
 namespace MoreGiantCrops
 {
     internal class Mod : StardewModdingAPI.Mod
@@ -26,6 +28,8 @@ namespace MoreGiantCrops
             Mod.Instance = this;
             Log.Monitor = this.Monitor;
 
+            helper.Events.GameLoop.GameLaunched += this.OnLaunched;
+
             var dir = Directory.CreateDirectory(Path.Combine(this.Helper.DirectoryPath, "assets"));
 
             Log.Trace("Finding giant crop images");
@@ -37,24 +41,108 @@ namespace MoreGiantCrops
                     Log.Error("Bad PNG: " + filename);
                     continue;
                 }
-                Mod.Sprites.Add(id, new (() => helper.ModContent.Load<Texture2D>($"assets/{filename}")));
+                Mod.Sprites.Add(id, new(() => helper.ModContent.Load<Texture2D>($"assets/{filename}")));
             }
 
-            Log.Trace($"{Sprites.Keys.Count} loaded, {string.Join(", ", Sprites.Keys.Select(k => k.ToString()))}");
+            Log.Trace($"{Sprites.Keys.Count} loaded from assets, {string.Join(", ", Sprites.Keys.Select(k => k.ToString()))}");
+
+            helper.ConsoleCommands.Add("force_load_giant_crops", "Forces MoreGiantCrops to load all textures", this.ForceLoad);
+
+            var harmony = HarmonyPatcher.Apply(this,
+                new CropPatcher(),
+                new GiantCropPatcher()
+            );
+
+            if (new Version(1, 6) > new Version(Game1.version))
+            {
+                this.Monitor.Log("Applying patch to restore giant crops from save");
+                var patch = new FixLocationsPatcher();
+                patch.Apply(harmony, this.Monitor);
+            }
+        }
+
+        private void OnLaunched(object sender, StardewModdingAPI.Events.GameLaunchedEventArgs e)
+        {
+            foreach (var pack in this.Helper.ContentPacks.GetOwned())
+            {
+                Log.Info($"Loading pack {pack.Manifest}");
+                try
+                {
+                    List<int> loaded = new();
+                    var data = pack.ReadJsonFile<List<CropModel>>("giantcrops.json");
+                    foreach (var datum in data)
+                    {
+                        if (datum.Index != -1 && datum.Path is not null)
+                        {
+                            if (!Mod.Sprites.TryAdd(datum.Index, new(() => pack.ModContent.Load<Texture2D>(datum.Path))))
+                            {
+                                Log.Warn($"Duplicate crop {datum.Index} not added");
+                            }
+                            else
+                            {
+                                loaded.Add(datum.Index);
+                            }
+                        }
+                    }
+
+                    Log.Trace($"Loaded {loaded.Count} giant crops: {string.Join(", ", loaded.Select((a) => a.ToString()))}");
+                }
+                catch (Exception ex)
+                {
+                    Log.Error($"Failed to load from {pack.Manifest}:\n\n{ex}");
+                }
+            }
 
             if (!Mod.Sprites.Any())
             {
                 Log.Error("You must install an asset pack to use this mod.");
                 return;
             }
-
-            HarmonyPatcher.Apply(this,
-                new CropPatcher(),
-                new GiantCropPatcher(),
-                new FixLocationsPatcher()
-            );
         }
 
         public override object GetApi() => new MoreGiantCropsAPI();
+
+        private void ForceLoad(string command, string[] param)
+        {
+            List<int> ToLoad;
+            if (param.Length == 0)
+            {
+                ToLoad = Sprites.Keys.ToList();
+            }
+            else
+            {
+                ToLoad = new(param.Length);
+                foreach (string val in param)
+                {
+                    if (int.TryParse(val, out int num))
+                    {
+                        ToLoad.Add(num);
+                    }
+                    else
+                    {
+                        this.Monitor.Log($"{val} is not a valid index.", LogLevel.Warn);
+                    }
+                }
+            }
+
+            foreach (int idx in ToLoad)
+            {
+                try
+                {
+                    if (Mod.Sprites.TryGetValue(idx, out var tex))
+                    {
+                        _ = tex.Value;
+                    }
+                    else
+                    {
+                        Log.Warn($"{idx} not found?");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Log.Error($"{idx} did not load correctly: {ex}");
+                }
+            }
+        }
     }
 }
