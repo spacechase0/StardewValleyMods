0bfca87e19ebc369512899294231c44846c6ffab
atravita-mods <94934860+atravita-mods@users.noreply.github.com>
2022-10-02 11:55:45 -0400

    Meow

--- MoreGiantCrops/Mod.cs
+++ MoreGiantCrops/Mod.cs
@@ -1,10 +1,16 @@
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+
 using Microsoft.Xna.Framework.Graphics;
+
 using MoreGiantCrops.Patches;
+
 using Spacechase.Shared.Patching;
+
 using SpaceShared;
+
 using StardewModdingAPI;
 
 namespace MoreGiantCrops
@@ -12,28 +18,29 @@ namespace MoreGiantCrops
     internal class Mod : StardewModdingAPI.Mod
     {
         public static Mod Instance;
-        public static Dictionary<int, Texture2D> Sprites = new();
+        public static Dictionary<int, Lazy<Texture2D>> Sprites = new();
 
+        /// <inheritdoc />
         public override void Entry(IModHelper helper)
         {
             Mod.Instance = this;
             Log.Monitor = this.Monitor;
 
-            Directory.CreateDirectory(Path.Combine(this.Helper.DirectoryPath, "assets"));
+            var dir = Directory.CreateDirectory(Path.Combine(this.Helper.DirectoryPath, "assets"));
 
             Log.Trace("Finding giant crop images");
-            foreach (string path in Directory.EnumerateFiles(Path.Combine(this.Helper.DirectoryPath, "assets"), "*.png"))
+            foreach (string path in Directory.EnumerateFiles(dir.FullName, "*.png"))
             {
-                string filename = Path.GetFileName(path);
-                if (!int.TryParse(filename.Split('.')[0], out int id))
+                string filename = Path.GetFileNameWithoutExtension(path);
+                if (!int.TryParse(filename, out int id))
                 {
                     Log.Error("Bad PNG: " + filename);
                     continue;
                 }
-                Log.Trace("Found PNG: " + filename);
-                var tex = helper.Content.Load<Texture2D>($"assets/{filename}");
-                Mod.Sprites.Add(id, tex);
+                Mod.Sprites.Add(id, new (() => helper.ModContent.Load<Texture2D>($"assets/{filename}")));
             }
+
+            Log.Trace($"{Sprites.Keys.Count} loaded, {string.Join(", ", Sprites.Keys.Select(k => k.ToString()))}");
 
             if (!Mod.Sprites.Any())
             {
@@ -43,8 +50,11 @@ namespace MoreGiantCrops
 
             HarmonyPatcher.Apply(this,
                 new CropPatcher(),
-                new GiantCropPatcher()
+                new GiantCropPatcher(),
+                new FixLocationsPatcher()
             );
         }
+
+        public override object GetApi() => new MoreGiantCropsAPI();
     }
 }
