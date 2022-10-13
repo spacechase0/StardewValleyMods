eb4ef50ac7cc6c4c6ef619f4ffecfd9759e14017
atravita-mods <94934860+atravita-mods@users.noreply.github.com>
2022-10-13 16:55:33 -0400

    remove no longer needed import.

--- ContentCode/Mod.cs
+++ ContentCode/Mod.cs
@@ -4,7 +4,6 @@ using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Text;
-using HarmonyLib;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using SpaceCore.Events;
