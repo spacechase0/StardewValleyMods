25930c3a4625039378a454e331a4ae3838c5e9cc
atravita-mods <94934860+atravita-mods@users.noreply.github.com>
2022-11-09 14:19:49 -0500

    i'm dumb

--- JsonAssets/Mod.cs
+++ JsonAssets/Mod.cs
@@ -2581,7 +2581,7 @@ namespace JsonAssets
                     this.FixItemList(shop.itemsFromPlayerToSell);
                     this.RemoveNulls(shop.itemsFromPlayerToSell);
                     this.FixItemList(shop.itemsToStartSellingTomorrow);
-                    this.RemoveNulls(shop.itemsToStartSellingTomorrow));
+                    this.RemoveNulls(shop.itemsToStartSellingTomorrow);
                     break;
             }
 
