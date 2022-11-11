1e85e9646c912a2ecfb72f29d4caef181134c9b0
Chase Warrington <spacechase0.and.cat@gmail.com>
2022-11-11 18:58:58 -0500

    AQM remove logging (1.0.1) and SpaceCore harmony_invalidate (for transpilers).

--- SpaceCore/SpaceCore.cs
+++ SpaceCore/SpaceCore.cs
@@ -65,7 +65,7 @@ namespace SpaceCore
         /*********
         ** Fields
         *********/
-        private Harmony Harmony;
+        internal Harmony Harmony;
 
         /// <summary>Handles migrating legacy data for a save file.</summary>
         private LegacyDataMigrator LegacyDataMigrator;
