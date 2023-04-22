f472fab7af878a78f2adad2b18b40c1c6edb74ed
Casey Warrington <spacechase0.and.cat@gmail.com>
2023-04-22 12:57:38 -0400

    Some schedule stuff I need to continue at some point

--- SpaceCore/Framework/Schedules/ScheduleDataPoint.cs
+++ SpaceCore/Framework/Schedules/ScheduleDataPoint.cs
@@ -7,17 +7,23 @@ using StardewValley;
 
 namespace SpaceCore.Framework.Schedules
 {
+    public class ScheduleSpot
+    {
+        public string Location { get; set; } = null;
+        public int TileX { get; set; }
+        public int TileY { get; set; }
+    }
+
     public class ScheduleDataPoint
     {
         public int Time { get; set; } = 600;
 
         public bool TimeIsEndingTime { get; set; } = false;
 
-        public string Location { get; set; } = null;
-        public int TileX { get; set; }
-        public int TileY { get; set; }
-
+        public ScheduleSpot StandingPosition;
         public int FacingDirection = Game1.down;
+
+        public List<ScheduleSpot> Waypoints = new();
 
         public string StartAnimationKey { get; set; }
         public string EndAnimationKey { get; set; }
