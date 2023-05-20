00f6fdcd1e60d6687efdd473a7dcf996e9e82a86
Casey Warrington <spacechase0.and.cat@gmail.com>
2023-05-20 15:02:48 -0400

    Stuff I forgot

--- DeepSeaFishing/IndustrialFishingRod.cs
+++ DeepSeaFishing/IndustrialFishingRod.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
+using StardewValley;
 
 namespace DeepSeaFishing
 {
-    internal class IndustrialFishingRod
+    [XmlType( "Mods_spacechase0_DeepSeaFishing_FishingRod" )]
+    internal class IndustrialFishingRod : Tool
     {
     }
 }
