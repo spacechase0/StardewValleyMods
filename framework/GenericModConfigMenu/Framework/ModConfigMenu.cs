khanshoaib3/spacechase0-stardew-valley-mods

Integrated Stardew Access to GMCM
Introduced two Event Handlers `MouseEntered` and `MouseHovered` to `Element.cs`.
MouseEntered is triggered when the mouse enters an element
and MouseHovered is triggered constantly while an element is being hovered.

GMCM uses `MouseHovered` to detect currently hovered element and attempts to speak, only if stardew access is installed.

GenericModConfigMenu/Framework/ModConfigMenu.cs
@@ -86,6 +86,7 @@ namespace GenericModConfigMenu.Framework
                         Label label = new Label
                         {
                             String = entry.ModName,
+                            ScreenReaderText = $"{entry.ModName}, {entry.ModManifest.Description}",
                             UserData = entry.ModManifest.Description,
                             Callback = _ => this.ChangeToModPage(entry.ModManifest)
                         };
@@ -137,6 +138,7 @@ namespace GenericModConfigMenu.Framework
             {
                 LocalPosition = this.Table.LocalPosition - new Vector2( keybindingsTex.Width / 2 + 32, 0 ),
                 Callback = _ => openKeybindingsMenu( this.ScrollRow),
+                ScreenReaderText = I18n.List_Keybindings(), // TODO Maybe add "Keybindings Menu" entry
             };
             this.Ui.AddChild(button);
 

spacechase0/StardewValleyMods

framework/GenericModConfigMenu/Framework/ModConfigMenu.cs
@@ -88,6 +88,7 @@ namespace GenericModConfigMenu.Framework
                         Label label = new Label
                         {
                             String = entry.ModName,
+                            ScreenReaderText = $"{entry.ModName}, {entry.ModManifest.Description}",
                             UserData = entry.ModManifest.Description,
                             Callback = _ => this.ChangeToModPage(entry.ModManifest)
                         };
@@ -139,6 +140,7 @@ namespace GenericModConfigMenu.Framework
             {
                 LocalPosition = this.Table.LocalPosition - new Vector2( keybindsTexture.Width / 2 + 32, 0 ),
                 Callback = _ => openKeybindsMenu( this.ScrollRow),
+                ScreenReaderText = I18n.List_Keybinds(), // TODO Maybe add "Keybindings Menu" entry
             };
             this.Ui.AddChild(button);
 