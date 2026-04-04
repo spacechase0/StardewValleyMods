6813019190f814c2b23242aef67d65d337717a51
Casey Warrington <spacechase0.and.cat@gmail.com>
2026-04-04 13:14:15 -0400

    Fix render cache reset shortcut

--- gameplay/overhaul/Stardew3D/Mod.cs
+++ gameplay/overhaul/Stardew3D/Mod.cs
@@ -236,7 +236,7 @@ namespace Stardew3D
 
 
             // TODO: hook up to keybind
-            if (e.Pressed.Contains(SButton.Delete) && e.Pressed.Contains(SButton.LeftControl))
+            if (e.Pressed.Contains(SButton.Delete) && e.Held.Contains(SButton.LeftControl))
             {
                 // Can clear render caches and stuff
                 State.ActiveMode?.SwitchOff(State.ActiveMode);
