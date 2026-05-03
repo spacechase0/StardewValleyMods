e064b04f864c1c786bbfecf3cb2003cb114089fa
Casey Warrington <spacechase0.and.cat@gmail.com>
2026-05-03 14:21:31 -0400

    Add farmhouse rendering

--- gameplay/overhaul/Stardew3D/Utilities/GameExtensions.cs
+++ gameplay/overhaul/Stardew3D/Utilities/GameExtensions.cs
@@ -2,6 +2,7 @@ using Microsoft.Xna.Framework;
 using StardewValley;
 using StardewValley.Buildings;
 using StardewValley.Extensions;
+using StardewValley.Locations;
 using StardewValley.Menus;
 using StardewValley.Monsters;
 using StardewValley.TerrainFeatures;
@@ -14,7 +15,7 @@ public static class GameExtenions
 {
     public static string GetExtendedQualifiedId(this object obj)
     {
-        return (obj?.GetExtendedQualifiedIds() ?? [null])[0];
+         return (obj?.GetExtendedQualifiedIds() ?? [null])[0];
     }
 
     public static string[] GetExtendedQualifiedIds(this object obj)
@@ -97,11 +98,22 @@ public static class GameExtenions
                 $"({Stardew3D.Mod.Instance.ModManifest.UniqueID}/Character)"
             ];
 
+        else if (obj is Building farmhouse && farmhouse.GetIndoors() is FarmHouse farmhouseInterior)
+            return
+            [
+                $"({Mod.Instance.ModManifest.UniqueID}/Building){farmhouse.id}",
+                $"({Mod.Instance.ModManifest.UniqueID}/Building){farmhouse.buildingType.Value}/{farmhouseInterior.upgradeLevel}/{farmhouse.skinId}",
+                $"({Mod.Instance.ModManifest.UniqueID}/Building){farmhouse.buildingType.Value}/{farmhouseInterior.upgradeLevel}",
+                $"({Mod.Instance.ModManifest.UniqueID}/Building){farmhouse.buildingType.Value}",
+                $"({Mod.Instance.ModManifest.UniqueID}/BuildingType){farmhouse.GetType().Name}",
+                $"({Mod.Instance.ModManifest.UniqueID}/Building)"
+            ];
         else if (obj is Building building)
             return
             [
-                $"({Mod.Instance.ModManifest.UniqueID}/Building){building.id}/{building.skinId}",
                 $"({Mod.Instance.ModManifest.UniqueID}/Building){building.id}",
+                $"({Mod.Instance.ModManifest.UniqueID}/Building){building.buildingType.Value}/{building.skinId}",
+                $"({Mod.Instance.ModManifest.UniqueID}/Building){building.buildingType.Value}",
                 $"({Mod.Instance.ModManifest.UniqueID}/BuildingType){building.GetType().Name}",
                 $"({Mod.Instance.ModManifest.UniqueID}/Building)"
             ];
