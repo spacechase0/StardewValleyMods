34dd17ac0c726318bd646de82472652c5f531811
Casey Warrington <spacechase0.and.cat@gmail.com>
2026-05-17 21:07:48 -0400

    Default viewport that will place UI properly when 2d zoom is set

--- gameplay/overhaul/Stardew3D/GameModes/BaseGameMode.cs
+++ gameplay/overhaul/Stardew3D/GameModes/BaseGameMode.cs
@@ -80,7 +80,7 @@ public abstract partial class BaseGameMode : IGameMode
 
         Game1.graphics.GraphicsDevice.Clear(ClearOptions.Target | ClearOptions.DepthBuffer | ClearOptions.Stencil, Color.CornflowerBlue, 1, 0);
 
-        Game1.viewport = new(0, 0, Game1.graphics.PreferredBackBufferWidth, Game1.graphics.PreferredBackBufferHeight);
+        Game1.viewport = new(0, 0, (int)(Game1.graphics.PreferredBackBufferWidth / Game1.options.zoomLevel), (int)(Game1.graphics.PreferredBackBufferHeight / Game1.options.zoomLevel));
         UpdateCamera();
 
         Game1.graphics.GraphicsDevice.RasterizerState = RenderHelper.RasterizerState;
